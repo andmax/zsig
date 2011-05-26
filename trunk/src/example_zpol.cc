5f65bce5efe31b0d3825e335fb664eb263ffda69
andmax <andmax@c87d298c-402d-ae0d-7a93-157c471917b5>
2011-05-26 21:23:30 +0000

    Zernike examples completed.

--- trunk/src/example_zpol.cc
+++ trunk/src/example_zpol.cc
@@ -63,7 +63,8 @@ void scalar_to_rgb( const ZERNIKE_VALUE_TYPE& scalar,
 int main( int argc, char *argv[] ) {
 
 	std::cout << "[zsig] Usage: " << argv[0] << " [write] (where write = 1 outputs Zernike images as ppm)\n"
-		  << "[zsig] First ZSIG example\n[zsig] Allocating memory for Zernike Polynomials Basis\n"
+		  << "[zsig] Example ** 1 ** Building Zernike Basis\n"
+		  << "[zsig] Allocating memory for Zernike Polynomials Basis\n"
 		  << "[zsig] The domain grid is: " << DOMAIN_GRID_X << " x " << DOMAIN_GRID_Y
 		  << " with Zernike Order = " << ZERNIKE_ORDER
 		  << " and each value type = " << sizeof(ZERNIKE_VALUE_TYPE) << " Bytes\n";
